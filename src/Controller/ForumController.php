28ac2c42ccc6386d6043b3dd6d83be399a86bad5
MathieuMolina <molina.mathieu@hotmail.com>
2023-03-21 10:12:12 +0100

    BASE OK, forum visuel basique, besoin rajout nouveau topic

--- src/Controller/ForumController.php
+++ src/Controller/ForumController.php
@@ -5,16 +5,17 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use App\Repository\TopicRepository;
 
 class ForumController extends AbstractController
 {
     #[Route('/forum', name: 'app_forum')]
-    public function index(): Response
+    public function  index(TopicRepository $topicRepository ): Response
     {
 
         return $this->render('forum/index.html.twig', [
             'controller_name' => 'ForumController',
-//            'topics' => $topicRepository -> findAll(),
+            'topics' => $topicRepository -> findAll(),
         ]);
     }
 }
