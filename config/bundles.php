3f53e3a8308bee259c0d295f20eb3246d34f0924
MathieuMolina <molina.mathieu@hotmail.com>
2023-05-04 09:43:44 +0200

    avant installation easyAdmin

--- config/bundles.php
+++ config/bundles.php
@@ -16,5 +16,4 @@ return [
     Symfony\WebpackEncoreBundle\WebpackEncoreBundle::class => ['all' => true],
     FOS\CKEditorBundle\FOSCKEditorBundle::class => ['all' => true],
     Knp\Bundle\PaginatorBundle\KnpPaginatorBundle::class => ['all' => true],
-    EasyCorp\Bundle\EasyAdminBundle\EasyAdminBundle::class => ['all' => true],
 ];
--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -9,17 +10,16 @@ use Symfony\Component\Routing\Annotation\Route;
 class HomeController extends AbstractController
 {
     #[Route('/', name: 'app_home')]
-    public function index(): Response
+    public function index(EntityManagerInterface $em): Response
     {
-    
-        if ($this->getUser()){
+
+        if ($this->getUser()) {
             return $this->redirectToRoute('app_centre');
         }
         //  ^ retourne l'utilisateur connecté afin de l'empêcher de retourner sur la page de connexion/inscription
-        
+
         return $this->render('home/index.html.twig', [
             'controller_name' => 'HomeController',
         ]);
-        
     }
 }
